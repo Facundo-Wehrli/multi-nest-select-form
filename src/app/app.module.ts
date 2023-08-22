72db526100364183d210b06dbec226fda11df045
Facundo <wehrlifacundo9@gmail.com>
2023-08-22 09:37:40 -0300

    chore: import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CountriesModule } from "./country/countries.module";
+import { CountriesModule } from './country/countries.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        CountriesModule
-    ]
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
 })
-export class AppModule { }
+export class AppModule {}
