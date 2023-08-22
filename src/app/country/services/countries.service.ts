d446c028b403021d15794f862e3c179d47810ee3
Facundo <wehrlifacundo9@gmail.com>
2023-08-22 09:39:34 -0300

    feat: get countries by region

--- src/app/country/services/countries.service.ts
+++ src/app/country/services/countries.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Region } from '../interfaces/country-interfaces';
+import { HttpClient } from '@angular/common/http';
+import { Region, SmallCountry } from '../interfaces/country-interfaces';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountriesService {
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
   private _regions: Region[] = [
     Region.Africa,
     Region.Asia,
@@ -13,9 +17,17 @@ export class CountriesService {
     Region.Oceania,
   ];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get regions(): Region[] {
     return [...this._regions];
+  }
+
+  getCountriesByRegion(region: Region): Observable<SmallCountry> {
+    const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+
+    return this.http
+      .get<SmallCountry>(url)
+      .pipe(tap((response) => console.log({ response })));
   }
 }
